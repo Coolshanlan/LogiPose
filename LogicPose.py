4bbce22a6a0322a785de2aa8ed07ff5ce7861f5c
Coolshanlan <joey0201020123@gmail.com>
2020-10-25 01:28:17 +0800

    add server GUI

--- LogicPose.py
+++ LogicPose.py
@@ -1,24 +1,45 @@
 import wx
 
 
+class ImageBox(wx.Panel):
+    def __init__(self, parent, filepath, id):
+        wx.Panel.__init__(self, parent, id)
+        imageFile = filepath
+        jpg1 = wx.Image(imageFile, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        self.SetSize(jpg1.GetWidth(), jpg1.GetHeight())
+        wx.StaticBitmap(self, -1, jpg1)
+
+        # wx.StaticBitmap(self, -1, jpg1, (10 + jpg1.GetWidth(), 5),
+        #                 (jpg1.GetWidth(), jpg1.GetHeight()))
+    def changeImage(self, filepath):
+        jpg1 = wx.Image(filepath, wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        wx.StaticBitmap(self, -1, jpg1)
+
+
 class Frame(wx.Frame):
     def __init__(self, title_name="defult", childframe=None):
         super(Frame, self).__init__(parent=None,
                                     title=title_name, size=(200, 200))
-        self.Center()
-        self.size = (500,500)
+        # self.Center()
+        self.SetSize(1000, 1000)
+        self.Maximize(True)
+        self.SetBackgroundColour('white')
         self.child = childframe
-        self.panel = wx.Panel(self)
-        self.btn = wx.Button(pos=(0, 0), size=(100, 100), parent=self.panel)
-        self.Bind(wx.EVT_BUTTON, self.button_click, self.btn)
+
+        self.createImageBox()
+
+    def createImageBox(self):
+        self.imagebox1 = ImageBox(self, "data\\sport.jpg", -1)
+        self.imagebox1.SetPosition((100, 100))
+        self.imagebox1.SetSize((100, 100))
 
     def button_click(self, event):
-        if self.child:
-            self.child.Close()
-        print("I'm Winner")
+        self.imagebox1.changeImage("data\\test.jpg")
 
 
 app = wx.App()
 frame1 = Frame()
+
+# frame1.SetSize(1000,1000)
 frame1.Show()
 app.MainLoop()
--- server.py
+++ server.py
@@ -1,23 +1,26 @@
-import wx 
+import wx
 from time import sleep
-import cv2 
+import cv2
 import socket
 import numpy as np
 import _pickle as pickle
-from threading import Thread 
+from threading import Thread
 from wx.lib.pubsub import pub as Publisher
+import ctypes
+user32 = ctypes.windll.user32
 
-class SocketThread(Thread): 
+
+class SocketThread(Thread):
     """Test Worker Thread Class."""
-        
-    def __init__(self): 
+
+    def __init__(self):
         """Init Worker Thread Class."""
-        Thread.__init__(self)     
+        Thread.__init__(self)
         self.addr = ("localhost", 6000)
-        
-    def run(self): 
+
+    def run(self):
         """Run Worker Thread."""
-        # This is the code executing in the new thread. 
+        # This is the code executing in the new thread.
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
         server.bind(self.addr)
@@ -35,38 +38,72 @@ class SocketThread(Thread):
             Publisher.sendMessage, "update", clientMessage
 
         conn.close()
-    
 
-class ShowCapture(wx.Panel): 
-    def __init__(self, parent, capture, fps=30): 
-        wx.Panel.__init__(self, parent) 
 
-        self.capture = capture 
-        ret, frame = self.capture.read() 
+class ShowCapture(wx.Panel):
+    def __init__(self, parent, capture, teachermod=0, fps=30):
+        wx.Panel.__init__(self, parent)
+        self.capture = capture
+        self.Linewidth = 5
+        self.moveerror = 50
+        self.teachermod = teachermod
+        if not teachermod:
+            self.Backpanel = wx.Panel(parent)
+        ret, frame = self.capture.read()
+        mag_X = self.Size[0]/frame.shape[1]
+        mag_Y = self.Size[1]/frame.shape[0]
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.resize(frame, None, fx=mag_X, fy=mag_Y)
+        self.bmp = wx.Bitmap.FromBuffer(
+            frame.shape[1], frame.shape[0], frame)
 
-        height, width = frame.shape[:2] 
-        parent.SetSize((width, height)) 
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
-
-        self.bmp = wx.Bitmap.FromBuffer(width, height, frame) 
-
-        self.timer = wx.Timer(self) 
+        self.timer = wx.Timer(self)
         self.timer.Start(1000./fps)
 
-        self.Bind(wx.EVT_PAINT, self.OnPaint) 
-        self.Bind(wx.EVT_TIMER, self.NextFrame) 
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.Bind(wx.EVT_TIMER, self.NextFrame)
         self.Bind(wx.EVT_ERASE_BACKGROUND, self.OnErase)
 
         self.Bind(wx.EVT_CLOSE, self.OnClose)
-
-        self.thread = SocketThread()
-        self.thread.setDaemon(True)
-        self.thread.start()
-        
+        if not teachermod:
+            self.changebackcolor(1)
         Publisher.subscribe(lambda self, msg: print(msg), "update")
 
-    def OnClose(self,event):
+    # def ResizeCapture(self, width, height):
+    #     self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    #     self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+
+    # def scale_bitmap(self, bitmap, width, height):
+    #     image = wx.ImageFromBitmap(bitmap)
+    #     image = image.Scale(width, height, wx.IMAGE_QUALITY_HIGH)
+    #     result = wx.BitmapFromImage(image)
+    #     return result
+    def changebackcolor(self, mode):
+        if mode == 1:
+            self.Backpanel.SetBackgroundColour("green")
+        else:
+            self.Backpanel.SetBackgroundColour("red")
+
+    def setsize(self, size):
+        if not self.teachermod:
+            self.SetSize(size)
+            self.Backpanel.SetSize(
+                wx.Size(size[0]+self.Linewidth*2, size[1]+self.Linewidth*2))
+        else:
+            self.SetSize(size)
+
+    def setposition(self, position):
+        if not self.teachermod:
+            self.SetPosition(
+                wx.Point(position[0]-self.Linewidth, position[1]-self.Linewidth))
+            self.Backpanel.SetPosition(
+                wx.Point(position[0]-self.Linewidth*2, position[1]-self.Linewidth*2))
+        else:
+            self.SetPosition(position)
+
+    def OnClose(self, event):
         self.thread.terminate()
+        self.capture.release()
         self.Destroy()
 
     def OnErase(self, event):
@@ -75,24 +112,67 @@ class ShowCapture(wx.Panel):
         pass
 
     def OnPaint(self, evt):
-        dc = wx.BufferedPaintDC(self) 
-        dc.DrawBitmap(self.bmp, 0, 0) 
+        dc = wx.BufferedPaintDC(self)
 
-    def NextFrame(self, event): 
-        ret, frame = self.capture.read() 
-        if ret: 
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) 
-            self.bmp.CopyFromBuffer(frame) 
+        dc.DrawBitmap(self.bmp, 0, 0)
+
+    def Gettailposition(self):
+        return wx.Point(self.Position[0]+self.Size[0]+self.Linewidth*2, 0)
+
+    def NextFrame(self, event):
+        ret, frame = self.capture.read()
+        if ret:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            mag_X = self.Size[0]/frame.shape[1]
+            mag_Y = self.Size[1]/frame.shape[0]
+            frame = cv2.resize(frame, None, fx=mag_X, fy=mag_Y)
+            self.bmp = wx.Bitmap.FromBuffer(
+                frame.shape[1], frame.shape[0], frame)
+            # self.bmp.CopyFromBuffer(frame)
             self.Refresh()
 
-if __name__ == '__main__':    
-    capture = None
-    capture = cv2.VideoCapture(2) 
-    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280) 
-    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720) 
 
-    app = wx.App() 
-    frame = wx.Frame(None) 
-    cap = ShowCapture(frame, capture) 
-    frame.Show() 
-    app.MainLoop() 
+if __name__ == '__main__':
+
+    capture = None
+    capture = cv2.VideoCapture(0)
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+    screen_width = user32.GetSystemMetrics(0)
+    screen_height = user32.GetSystemMetrics(1)
+
+    capture2 = cv2.VideoCapture(2)
+    capture2.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    capture2.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    capture3 = cv2.VideoCapture(3)
+    capture3.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    capture3.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+    app = wx.App()
+    frame = wx.Frame(None)
+    frame.SetBackgroundColour('white')
+    frame.Maximize(True)
+    teachercap = ShowCapture(frame, capture, teachermod=1)
+    mainstudentcap = ShowCapture(frame, capture)
+    secondstudentcap = ShowCapture(frame, capture2)
+    thirdstudentcap = ShowCapture(frame, capture3)
+
+    frame.Show()
+    socketthread = SocketThread()
+    socketthread.setDaemon(True)
+    socketthread.start()
+    teachercap.setsize(wx.Size((1280//2.3, 720//2.4)))
+    teachercap.setposition(
+        wx.Point(screen_width-teachercap.Size[0], screen_height-teachercap.Size[1]-20))
+    mainstudentcap.setsize(wx.Size((1280//1.8, 720//1.9)))
+    mainstudentcap.setposition(
+        wx.Point(0+mainstudentcap.Linewidth*2, screen_height-mainstudentcap.Size[1]-22))
+    secondstudentcap.setsize(wx.Size((1280//3.2, 720//2.6)))
+    secondstudentcap.setposition(
+        wx.Point(0+mainstudentcap.Linewidth*2, 0+mainstudentcap.Linewidth*2))
+    thirdstudentcap.setsize(wx.Size((1280//3.2, 720//2.6)))
+    thirdstudentcap.setposition(
+        wx.Point(0+mainstudentcap.Linewidth*2+secondstudentcap.Gettailposition()[0], 0+mainstudentcap.Linewidth*2+secondstudentcap.Gettailposition()[1]))
+
+app.MainLoop()
